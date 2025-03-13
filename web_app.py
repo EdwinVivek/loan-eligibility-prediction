e1d70fefa41e625d27ec5297048119eac204673d
EdwinVivek <nedwinvivek@gmail.com>
2025-03-13 19:16:22 +0530

    added web interface for the model

---
+++ web_app.py
@@ -0,0 +1,41 @@
+import joblib
+from flask import Flask, request, jsonify, render_template
+import pandas as pd
+from custom_transformers import SimpleImputerWithMapping, CustomBinning
+import numpy as np
+import pandas as pd
+import os
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def home():
+    return render_template("home.html", prediction=None, error=None), 200
+
+@app.route("/predict", methods=["GET","POST"])
+def predict():
+    if request.method == 'POST':
+        print("hello")
+        try:
+            data = request.form  
+            loan = pd.read_csv(os.path.abspath(os.path.join(os.getcwd(), 'data\\train_loanpred.csv')), nrows=1, usecols=lambda x: x not in ["Loan_ID", "Loan_Status"])
+            numeric_cols = loan.select_dtypes(include=['int64', 'float64']).columns.to_list()
+            features = loan.columns.to_list()
+            response = {key: float(data[key]) if key in numeric_cols else data[key] for key in features}
+            #response = {key: data[key] for key in features} 
+            #response = np.array(response).reshape(1, -1)
+            lr_model = joblib.load("logistic.pkl")
+            df = pd.DataFrame([response])
+            prediction = lr_model.predict(df)
+            result = 'Approved' if str(prediction)[2] == 'Y' else 'Rejected'
+            return render_template('home.html', prediction=result)
+        except Exception as e:
+            return render_template('home.html', error=str(e))
+    if request.method == 'GET':
+        return render_template('home.html',  prediction=None, error=None)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5555)
+
